3fbc2eb1ddb0692363056734097ae1d125f8e170
--include <harpercscott@gmail.com>
2025-01-11 00:01:24 -0500

    Update Neovim configuration

--- neovim/nvim/lua/plugins/dashboard.lua
+++ neovim/nvim/lua/plugins/dashboard.lua
@@ -5,6 +5,15 @@ return {
     dependencies = { {'nvim-tree/nvim-web-devicons'} },
 
     config = function()
+      local date_time = os.date('󰃰 %A, %d %B %Y | %I:%M %p')
+
+      local align_center = function(ref_text, text)
+        padding = #ref_text - #text
+        left_pad = math.floor(padding / 2)
+        right_pad = (padding - left_pad)
+        return string.rep(' ', left_pad) .. text .. string.rep(' ', right_pad)
+      end
+
       vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#CCC7B4' })
       vim.api.nvim_set_hl(0, 'DashboardFooter', { fg = '#807A58' })
 
@@ -12,14 +21,15 @@ return {
         theme = 'hyper',
         shortcut_type = 'number',
 
-        -- for whatever reason this applies even after leaving the dashboard
-        -- current fix is to disable hiding the statusline on the dashboard
+        -- handle hiding and unhiding statusline with an autocmd
         hide = {
           statusline = false,
         },
 
         config = {
           header = {
+            [[                                                 ]],
+            [[                                                 ]],
             [[                                                 ]],
             [[                                                 ]],
             [[      _______                        /\          ]],
@@ -30,6 +40,7 @@ return {
             [[\____\  /\_____/\_____/  \__/___/________\/     \]],
             [[      \/                                  \_____/]],
             [[                                                 ]],
+            align_center('                                                 ', date_time),
           },
 
           shortcut = {
@@ -59,23 +70,51 @@ return {
             },
           },
 
-          packages = { enable = true },
+          packages = { enable = false },
           project = { enable = false },
           mru = {
             enable = true,
             limit = 5,
           },
 
-          footer = {
-            [[                                                     ]],
-            [[                                                     ]],
-            [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢻⣀⣧⣤⣽⣦⣤⣄⠀   ⠀⠀⠀⠀⠀⠀⡴⠟⠛⣉⣉⡉⠉⠈⠉⠉⠉⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀]],
-            [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠖⠛⠉⠉⠉⠀⠀⠐⠒⢢⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⣠⣲⣾⣿⢿⣷⢶⡄⠀⠀⣽⣿⣿⣿⣿⡿⠟⣿⣿ ]],
-            [[⢽⣿⣿⣿⣿⣿⣿⣿⣿⡷⠀⠀⠀⣠⣶⣶⣿⣟⣿⣶⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⢿⣿⣿⣿⣿⠀⣿⡀⠀⢻⣬⣙⡻⡿⣡⣾⣿⣿ ]],
-            [[ ⢻⣫⢿⣿⣿⣿⣿⠘⢧⠁⠀⣻⡏⠸⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⣉⣛⣋⣡⡴⠃⠀⠀⣿⣿⣿⠟⣠⡛⢿⣿⣿⣷]],
-            [[    ⣿⢿⣿⣿⣟⡆⠀⠀⠉⠿⣦⣈⣉⣉⠤⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡟⣡⣶⣿⣿⣾⣿⣿⣿]],
-          },
+          footer = function()
+            local padding = '                                                     '
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            local version = ' ' .. vim.version().major .. '.' .. vim.version().minor .. '.' .. vim.version().patch
+            return {
+              [[                                                     ]],
+              [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⢻⣀⣧⣤⣽⣦⣤⣄⠀   ⠀⠀⠀⠀⠀⠀⡴⠟⠛⣉⣉⡉⠉⠈⠉⠉⠉⠋⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀]],
+              [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠖⠛⠉⠉⠉⠀⠀⠐⠒⢢⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⣠⣲⣾⣿⢿⣷⢶⡄⠀⠀⣽⣿⣿⣿⣿⡿⠟⣿⣿ ]],
+              [[⢽⣿⣿⣿⣿⣿⣿⣿⣿⡷⠀⠀⠀⣠⣶⣶⣿⣟⣿⣶⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⢿⣿⣿⣿⣿⠀⣿⡀⠀⢻⣬⣙⡻⡿⣡⣾⣿⣿ ]],
+              [[ ⢻⣫⢿⣿⣿⣿⣿⠘⢧⠁⠀⣻⡏⠸⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⣉⣛⣋⣡⡴⠃⠀⠀⣿⣿⣿⠟⣠⡛⢿⣿⣿⣷]],
+              [[    ⣿⢿⣿⣿⣟⡆⠀⠀⠉⠿⣦⣈⣉⣉⠤⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡟⣡⣶⣿⣿⣾⣿⣿⣿]],
+              [[                                                     ]],
+              align_center(padding, stats.loaded .. '/' .. stats.count .. ' Plugins Loaded in ' .. ms .. 'ms'),
+              align_center(padding, version),
+            }
+          end,
         },
+      })
+
+      vim.api.nvim_create_augroup('dashboard_statusline', { clear = true })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = 'dashboard_statusline',
+        pattern = 'dashboard',
+        command = 'set laststatus=0',
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = 'dashboard_statusline',
+        pattern = 'dashboard',
+        callback = function()
+          vim.api.nvim_create_autocmd('BufUnload', {
+            group = 'dashboard_statusline',
+            buffer = 0,
+            command = 'set laststatus=2',
+          })
+        end,
       })
     end,
   },
