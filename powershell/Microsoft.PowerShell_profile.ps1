7b07299cace8a626a29c3234a8049eb0061b1149
--include <harpercscott@gmail.com>
2025-01-22 18:25:29 -0500

    Add comment to PowerShell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,2 @@
+# initialize oh my posh and set theme
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/robbyrussell.omp.json' | Invoke-Expression
