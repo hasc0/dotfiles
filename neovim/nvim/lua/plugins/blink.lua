745c30ea2ca5f6ca762e6e8723616195f7f8d3e0
Harper Scott <harpercscott@gmail.com>
2025-03-27 12:47:03 -0400

    Update Neovim configuration

--- neovim/nvim/lua/plugins/blink.lua
+++ neovim/nvim/lua/plugins/blink.lua
@@ -6,7 +6,12 @@ return {
     opts = {
       keymap = { preset = 'enter' },
       appearance = { nerd_font_variant = 'normal' },
-      completion = { documentation = { auto_show = false } },
+
+      completion = {
+        documentation = { auto_show = false },
+        ghost_text = { enabled = true },
+      },
+
       sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
     },
   },
