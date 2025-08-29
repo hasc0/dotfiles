6bc8d7bc5c57d7845c849c54baabcb5ff62d84a2
Harper Scott <harpercscott@gmail.com>
2025-08-29 19:31:10 -0400

    Update Neovim configuration

--- neovim/nvim/lua/plugins/markview.lua
+++ neovim/nvim/lua/plugins/markview.lua
@@ -2,5 +2,7 @@ return {
   {
     'OXY2DEV/markview.nvim',
     lazy = false,
+    priority = 49,
+    dependencies = { 'saghen/blink.cmp' },
   },
 }
--- neovim/nvim/lua/plugins/treesitter.lua
+++ neovim/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = { 'OXY2DEV/markview.nvim' },
 
     opts = {
       ensure_installed = {
